863b2ca349f650e42cf2405993389d6cdab60e94
Greg <harrison.greg.c@gmail.com>
2016-02-05 08:12:10 -0500

    Added fallback to Laptop screen in my xrandr setup config.

--- screenlayout/Both-Work-Monitors.sh
+++ screenlayout/Both-Work-Monitors.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 752x1920 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --off --output DP2-3 --off --output HDMI2 --primary --mode 1920x1200 --pos 1200x368 --rotate normal --output HDMI1 --mode 1920x1200 --pos 0x0 --rotate left --output DP2 --off
+
+if (xrandr | grep "HDMI1 disconnected") || (xrandr | grep "HDMI2 disconnected") ; then
+  xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+else
+  xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --off --output HDMI2 --primary --mode 1920x1200 --pos 1200x368 --rotate normal --output HDMI1 --mode 1920x1200 --pos 0x0 --rotate left --output DP2 --off
+fi
