e957236b97e479c171e6a3a160b9680e21f1d5f5
Greg <harrison.greg.c@gmail.com>
2016-02-09 14:43:35 -0500

    Added a bash script for a single monitor

---
+++ screenlayout/single-monitor.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if (xrandr | grep "HDMI1 disconnected") || (xrandr | grep "HDMI2 disconnected") ; then
+  killall conky
+  xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+  conky
+else
+  killall conky
+  xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --off --output HDMI2 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI1 --off --output DP2 --off
+  conky
+fi
