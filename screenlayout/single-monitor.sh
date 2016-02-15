b8ca68717a7d9418ed9293910900ca0d5c7c7740
Greg <harrison.greg.c@gmail.com>
2016-02-15 14:21:00 -0500

    Updated Dconf

--- screenlayout/single-monitor.sh
+++ screenlayout/single-monitor.sh
@@ -6,6 +6,6 @@ if (xrandr | grep "HDMI1 disconnected") && (xrandr | grep "HDMI2 disconnected") 
   conky
 else
   killall conky
-  xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --off --output HDMI2 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI1 --off --output DP2 --off
+  xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output DP1 --off --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --off --output DP2 --off
   conky
 fi
