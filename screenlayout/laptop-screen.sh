55e55b753d44d0361808178646db00a2f4f0f5eb
Greg <harrison.greg.c@gmail.com>
2016-03-15 10:48:59 -0400

    Added support for Plank to screenLayout exe's

--- screenlayout/laptop-screen.sh
+++ screenlayout/laptop-screen.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 killall conky
+killall plank
 xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
 conky
+plank
