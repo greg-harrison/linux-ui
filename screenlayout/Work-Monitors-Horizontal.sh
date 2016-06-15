d8f0e4e017fc50b5b17d3405fcb97b322230c2f1
Greg <harrison.greg.c@gmail.com>
2016-06-15 13:51:18 -0400

    Added horizontal monitors xrandr script

---
+++ screenlayout/Work-Monitors-Horizontal.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if (xrandr | grep "HDMI1 disconnected") || (xrandr | grep "HDMI2 disconnected") ; then
+  killall conky
+  killall plank
+  xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+  conky
+  plank
+else
+  killall conky
+  killall plank
+  xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --off --output HDMI2 --primary --mode 1920x1200 --pos 1920x0 --rotate normal --output HDMI1 --mode 1920x1200 --pos 0x0 --rotate normal --output DP2 --off
+  conky
+  plank
+fi
+
