4deed363028aa2247c7d5c8431059e4cca168196
Greg <harrison.greg.c@gmail.com>
2016-06-15 14:03:41 -0400

    Updated Horizontal Monitor Script for Samsung device

--- screenlayout/Work-Monitors-Horizontal.sh
+++ screenlayout/Work-Monitors-Horizontal.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
-if (xrandr | grep "HDMI1 disconnected") || (xrandr | grep "HDMI2 disconnected") ; then
+if (xrandr | grep "DP1 disconnected") || (xrandr | grep "HDMI1 disconnected") ; then
   killall conky
-  killall plank
   xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
   conky
-  plank
 else
   killall conky
-  killall plank
-  xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --off --output HDMI2 --primary --mode 1920x1200 --pos 1920x0 --rotate normal --output HDMI1 --mode 1920x1200 --pos 0x0 --rotate normal --output DP2 --off
+  xrandr --output VIRTUAL1 --off --output eDP1 --off --output HMDI2 --off --output DP1 --primary --mode 1920x1200 --pos 1920x0 --rotate normal --output HMDI1 --mode 1920x1200 --pos 0x0 --rotate normal --output DP2 --off
   conky
-  plank
 fi
 
