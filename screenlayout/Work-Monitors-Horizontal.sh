1ea3bf7c4f8e072483b19e7d2d01fc76c7165809
Greg <harrison.greg.c@gmail.com>
2016-08-03 11:34:45 -0400

    Added single monitor case to horizontal script

--- screenlayout/Work-Monitors-Horizontal.sh
+++ screenlayout/Work-Monitors-Horizontal.sh
@@ -4,6 +4,10 @@ if (xrandr | grep "DP1 disconnected") || (xrandr | grep "HDMI1 disconnected") ; 
   killall conky
   xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
   conky
+elif (xrandr | grep "HDMI1 connected") && (xrandr | grep "DP1 disconnected") ; then
+  killall conky
+  xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --off --output HDMI2 --off --output HDMI1 --primary --mode 1920x1200 --pos 0x0 --rotate normal  --output DP2 --off
+  conky
 else
   killall conky
   xrandr --output VIRTUAL1 --off --output eDP1 --off --output HMDI2 --off --output DP1 --primary --mode 1920x1200 --pos 1920x0 --rotate normal --output HMDI1 --mode 1920x1200 --pos 0x0 --rotate normal --output DP2 --off
