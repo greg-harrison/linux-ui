4baa4b99c4b6c445e4b14f583c78f99c0a32c3be
Greg <harrison.greg.c@gmail.com>
2016-08-09 14:29:24 -0400

    Added a line for science

--- screenlayout/single-monitor.sh
+++ screenlayout/single-monitor.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+
 if (xrandr | grep "HDMI1 disconnected") && (xrandr | grep "HDMI2 disconnected") ; then
   killall conky
   xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
